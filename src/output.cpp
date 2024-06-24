e17c5fcd7f40e81d6aec84ae95939f59cd2d8ad4
= <=>
2024-06-24 22:39:49 +0300

    Fixed annoying indentation

--- src/output.cpp
+++ src/output.cpp
@@ -6,22 +6,22 @@
 int output_code(std::string transpiled_code, std::string destination)
 {
 	std::string temp_file_name = "temp_file_for_bfc_mid_compile.c";
-    std::ofstream temp_file(temp_file_name);
+	std::ofstream temp_file(temp_file_name);
 
-    if (!temp_file) 
-    {
-        std::cerr << "Could not open the temp file for writing." << std::endl;
-        return 1;
-    }
+	if (!temp_file) 
+	{
+		std::cerr << "Could not open the temp file for writing." << std::endl;
+		return 1;
+	}
 
-    temp_file << transpiled_code;
-    temp_file.close();
+	temp_file << transpiled_code;
+	temp_file.close();
 
-    std::string gcc_command = "g++ -o " + destination + " ";
-    gcc_command += temp_file_name;
+	std::string gcc_command = "g++ -o " + destination + " ";
+	gcc_command += temp_file_name;
 
-    std::system(gcc_command.c_str());
-    std::remove(temp_file_name.c_str());
+	std::system(gcc_command.c_str());
+	std::remove(temp_file_name.c_str());
 
 	return 0;
 }
